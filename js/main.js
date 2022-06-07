45485514bc5479106bae4345a1a75a7954f5f3fc
ValeraGalera <nuberman88@gmail.com>
2022-06-07 15:24:20 +0400

    ф-я checkLengthText в короткую запись

--- js/main.js
+++ js/main.js
@@ -25,16 +25,6 @@ getRandomOnlyPositiveInt(2, 2.2);
 const TEXT = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut a provident cum fuga. Accusantium aut perspiciatis temporibus ipsam, rem, error porro, deserunt ratione rerum ab quod veritatis. Incidunt, molestiae eos?';
 
 //Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария
-const checkLengthText = (text='hello', max=140) => {
-  let isLongText = true;
-
-  if (text.length > max) {
-    isLongText = false;
-    return isLongText;
-  }
-
-  isLongText = true;
-  return isLongText;
-};
+const checkLengthText = (text = 'hello', max = 140) => text.length <= max;
 
 checkLengthText(TEXT, 140);
