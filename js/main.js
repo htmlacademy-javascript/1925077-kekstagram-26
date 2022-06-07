6d3b3c6bef4ae6cf982e17c736d1b932ba4f8427
ValeraGalera <nuberman88@gmail.com>
2022-06-07 16:40:40 +0400

    исправления после повторной проверки module2-task1

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
 const getRandomOnlyPositiveInt = (min=2, max=3) => {
+  let randomInt;
 
   if (min < 0 || max < 0) {
     return ('Оба числа должны быть больше либо равным нулю');
   }
 
-  if (min > max) {
-    const x = min;
-    min = max;
-    max = x;
-  }
-
   if (Math.round(min) === Math.round(max)) {
-    max++;
+    randomInt = min + Math.random() * (max + 1 - min);
+    return Math.round(randomInt);
   }
 
-  //Источник кода: https://learn.javascript.ru/task/random-int-min-max
-  const randomInt = min + Math.random() * (max - min);
+  if (min > max) {
+    randomInt = max + Math.random() * (min - max);
+    return (Math.round(randomInt));
+  }
+
+  randomInt = min + Math.random() * (max - min);
   return (Math.round(randomInt));
 };
 
-getRandomOnlyPositiveInt(2, 2.2);
+getRandomOnlyPositiveInt(3, 2.2);
 
 const TEXT = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut a provident cum fuga. Accusantium aut perspiciatis temporibus ipsam, rem, error porro, deserunt ratione rerum ab quod veritatis. Incidunt, molestiae eos?';
 
