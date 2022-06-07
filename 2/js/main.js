4e17a4decf22d055a4fcb2ed5c504d8e6c664cbc
Keks <keks@htmlacademy.ru>
2022-06-07 14:32:17 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,5 +1,5 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
-function getRandomOnlyPositiveInt (min=2, max=3) {
+const getRandomOnlyPositiveInt = (min=2, max=3) => {
 
   if (min < 0 || max < 0) {
     return ('Оба числа должны быть больше либо равным нулю');
@@ -18,23 +18,13 @@ function getRandomOnlyPositiveInt (min=2, max=3) {
   //Источник кода: https://learn.javascript.ru/task/random-int-min-max
   const randomInt = min + Math.random() * (max - min);
   return (Math.round(randomInt));
-}
+};
 
 getRandomOnlyPositiveInt(2, 2.2);
 
 const TEXT = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut a provident cum fuga. Accusantium aut perspiciatis temporibus ipsam, rem, error porro, deserunt ratione rerum ab quod veritatis. Incidunt, molestiae eos?';
 
 //Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария
-function checkLengthText(text='hello', max=140) {
-  let isLongText = true;
-
-  if (text.length > max) {
-    isLongText = false;
-    return isLongText;
-  }
-
-  isLongText = true;
-  return isLongText;
-}
+const checkLengthText = (text = 'hello', max = 140) => text.length <= max;
 
 checkLengthText(TEXT, 140);
