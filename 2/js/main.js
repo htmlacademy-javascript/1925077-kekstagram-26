86a32fdff65b4131375727ddc0037cdd95ec2ffc
Keks <keks@htmlacademy.ru>
2022-06-07 13:18:05 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,37 +1,33 @@
-// 'use strict'; // Нужна ли сейчас эта директива?
-
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
-function randomizeInt (min, max) {
+function getRandomOnlyPositiveInt (min=2, max=3) {
 
   if (min < 0 || max < 0) {
     return ('Оба числа должны быть больше либо равным нулю');
   }
-  //Или можно каждое число приравнять к его абсолютному значению?
-
-  min = Math.round(min);
-  max = Math.round(max);
 
   if (min > max) {
-    const X = min;
+    const x = min;
     min = max;
-    max = X;
-  } else if (min === max) {
+    max = x;
+  }
+
+  if (Math.round(min) === Math.round(max)) {
     max++;
   }
 
   //Источник кода: https://learn.javascript.ru/task/random-int-min-max
-  const randomInt = min + Math.random() * (max + 1 - min);
-  return (Math.floor(randomInt));
+  const randomInt = min + Math.random() * (max - min);
+  return (Math.round(randomInt));
 }
 
-randomizeInt(2, 2.3);
+getRandomOnlyPositiveInt(2, 2.2);
 
 const TEXT = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut a provident cum fuga. Accusantium aut perspiciatis temporibus ipsam, rem, error porro, deserunt ratione rerum ab quod veritatis. Incidunt, molestiae eos?';
 
 //Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария
-function checkLengthText(text, max) {
+function checkLengthText(text='hello', max=140) {
   let isLongText = true;
-  // return text.length > max ? isLongText = false : isLongText = true;//errors: Return statement should not contain assignment   no-return-assign | 'isLongText' is assigned a value but never used  no-unused-vars
+
   if (text.length > max) {
     isLongText = false;
     return isLongText;
