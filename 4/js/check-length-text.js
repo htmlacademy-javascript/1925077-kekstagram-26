9e2612efc34a0eb50056414739231af8e092edac
Keks <keks@htmlacademy.ru>
2022-06-18 19:55:29 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/check-length-text.js
@@ -0,0 +1,6 @@
+export const TEXT = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut a provident cum fuga. Accusantium aut perspiciatis temporibus ipsam, rem, error porro, deserunt ratione rerum ab quod veritatis. Incidunt, molestiae eos?';
+
+//Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария
+export const checkLengthText = (text = 'hello', max = 140) => text.length <= max;
+
+checkLengthText(TEXT, 140);
---
+++ 4/js/main.js
@@ -0,0 +1,6 @@
+import {getDescriptionsOfPhoto} from './get-descriptions-of-photo.js';
+import {checkLengthText, TEXT} from './check-length-text.js';
+
+checkLengthText(TEXT, 140);
+
+getDescriptionsOfPhoto();
