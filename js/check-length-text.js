a57daf9e6b352650ebd3bf60a5c1adc42be0e67c
ValeraGalera <nuberman88@gmail.com>
2022-06-19 10:16:05 +0400

    завели конст MAX_LENGTH - макс длина проверяемой строки

--- js/check-length-text.js
+++ js/check-length-text.js
@@ -1,6 +1,7 @@
 export const TEXT = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut a provident cum fuga. Accusantium aut perspiciatis temporibus ipsam, rem, error porro, deserunt ratione rerum ab quod veritatis. Incidunt, molestiae eos?';
 
+export const MAX_LENGTH = 140;
+
 //Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария
 export const checkLengthText = (text = 'hello', max = 140) => text.length <= max;
 
-checkLengthText(TEXT, 140);
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {getDescriptionsOfPhoto} from './get-descriptions-of-photo.js';
-import {checkLengthText, TEXT as TEXT_FOR_CHECK_LENGTH} from './check-length-text.js';
+import {checkLengthText, TEXT as TEXT_FOR_CHECK_LENGTH, MAX_LENGTH} from './check-length-text.js';
 
-checkLengthText(TEXT_FOR_CHECK_LENGTH, 140);
+// checkLengthText(TEXT_FOR_CHECK_LENGTH, MAX_LENGTH);
+console.log(checkLengthText(TEXT_FOR_CHECK_LENGTH, MAX_LENGTH));
 
 getDescriptionsOfPhoto();
