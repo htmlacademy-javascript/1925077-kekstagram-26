be1a12cbdb1545fd9c0bb7c927577ba73086a49b
ValeraGalera <nuberman88@gmail.com>
2022-06-29 11:13:23 +0400

    отрисованы кол-во лайков и комментов

--- js/render-miniatures.js
+++ js/render-miniatures.js
@@ -1,18 +1,21 @@
 import {getDescriptionsOfPhoto} from './get-descriptions-of-photo.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const picturesList = document.querySelector('.pictures');
+const picturesContainer = document.querySelector('.pictures');
 
 
 const descriptions = getDescriptionsOfPhoto();
+const comments = descriptions.comments;
 
 descriptions.forEach((miniature)=>{
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = miniature.url;
-  picturesList.append(pictureElement);
+  pictureElement.querySelector('.picture__comments').textContent = miniature.comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = miniature.likes;
+  picturesContainer.append(pictureElement);
 });
 
 // console.log(pictureTemplate);
-console.log(picturesList);
+// console.log(picturesContainer);
 // console.log(pictureElement);
-console.log(descriptions);
+console.log(comments);
