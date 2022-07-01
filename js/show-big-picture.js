125e045e5031ef4621fd68960bd5d41f10e8266b
ValeraGalera <nuberman88@gmail.com>
2022-07-01 10:21:30 +0400

    отображаются комменты только вместе с захардкоженными

--- js/show-big-picture.js
+++ js/show-big-picture.js
@@ -1,15 +1,45 @@
 import {photosWithData} from './render-miniatures.js';
 
+const idOfPhoto = 0;
+
 const bigPictureTemplate = document.querySelector('.big-picture');
 bigPictureTemplate.classList.remove('hidden');
 
-bigPictureTemplate.querySelector('.js-full-img').src = photosWithData[0].url;
-bigPictureTemplate.querySelector('.js-full-img').alt = photosWithData[0].description;
-bigPictureTemplate.querySelector('.social__caption').textContent = photosWithData[0].description;
-bigPictureTemplate.querySelector('.likes-count').textContent = photosWithData[0].likes;
+const listofCommentsTemplate = bigPictureTemplate.querySelector('.social__comments');
+const commentTemplate = bigPictureTemplate.querySelector('.social__comment');
+// const commentElement = bigPictureTemplate.querySelectorAll('.social__comment').cloneNode(true);
+const hardComments = bigPictureTemplate.querySelectorAll('.social__comment');
+const comments = photosWithData[idOfPhoto].comments;
+const fragment = document.createDocumentFragment();
+
+
+bigPictureTemplate.querySelector('.js-full-img').src = photosWithData[idOfPhoto].url;
+bigPictureTemplate.querySelector('.js-full-img').alt = photosWithData[idOfPhoto].description;
+bigPictureTemplate.querySelector('.social__caption').textContent = photosWithData[idOfPhoto].description;
+bigPictureTemplate.querySelector('.likes-count').textContent = photosWithData[idOfPhoto].likes;
+
+// const fragment
+
+// listofCommentsTemplate.remove();//Удаление жестко отрисованных комментарев в разметке
+comments.forEach((comment) => {
+  const commentElement = commentTemplate.cloneNode(true);
+  commentElement.querySelector('.social__picture').src = comment.avatar;
+  commentElement.querySelector('.social__text').textContent = comment.message;
+  console.log(commentElement);
+  listofCommentsTemplate.append(commentElement);
+});
+
+// listofCommentsTemplate.append(fragment);
+
+// commentElement.querySelector('.social__picture').src = 'img/avatar-6.svg';
+// commentElement.querySelector('.social__text').textContent = 'img/avatar-3.svg';
+
+// listofCommentsTemplate.append(commentElement);
 
 
 
-console.log('************************');
-console.log(photosWithData[0]);
-console.log('************************');
+
+// console.log('************************');
+// console.log(photosWithData[0]);
+// console.log(commentTemplate);
+// console.log(comments);
