d6d2ece8f5c002c66aa9ddd8e6467d8b8951838c
ValeraGalera <nuberman88@gmail.com>
2022-07-01 13:46:22 +0400

    временное скрытие комментов и их кол-ва на фулл-пике

--- js/show-big-picture.js
+++ js/show-big-picture.js
@@ -2,58 +2,50 @@ import {photosWithData} from './render-miniatures.js';
 
 const idOfPhoto = 0;
 
-const bigPictureTemplate = document.querySelector('.big-picture');
-bigPictureTemplate.classList.remove('hidden');
+const bigPicture = document.querySelector('.big-picture');
+bigPicture.classList.remove('hidden');
 
-const listofCommentsTemplate = bigPictureTemplate.querySelector('.social__comments');
-const commentTemplate = bigPictureTemplate.querySelector('.social__comment');
-// const commentElement = bigPictureTemplate.querySelectorAll('.social__comment').cloneNode(true);
-const hardComments = bigPictureTemplate.querySelectorAll('.social__comment');
+const listofCommentsTemplate = bigPicture.querySelector('.social__comments');
+
+/////////////////////////////////////////////////////////////////////////////
+bigPicture.querySelector('.social__comment-count').classList.add('hidden');//
+listofCommentsTemplate.classList.add('hidden');//      <=========      //////
+/////////          временное скрытие          ///////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+const commentTemplate = bigPicture.querySelector('.social__comment');
 const comments = photosWithData[idOfPhoto].comments;
 const fragment = document.createDocumentFragment();
+const closingButton = bigPicture.querySelector('.big-picture__cancel');
 
 
-bigPictureTemplate.querySelector('.js-full-img').src = photosWithData[idOfPhoto].url;
-bigPictureTemplate.querySelector('.js-full-img').alt = photosWithData[idOfPhoto].description;
-bigPictureTemplate.querySelector('.social__caption').textContent = photosWithData[idOfPhoto].description;
-bigPictureTemplate.querySelector('.likes-count').textContent = photosWithData[idOfPhoto].likes;
+bigPicture.querySelector('.js-full-img').src = photosWithData[idOfPhoto].url;
+bigPicture.querySelector('.js-full-img').alt = photosWithData[idOfPhoto].description;
+bigPicture.querySelector('.social__caption').textContent = photosWithData[idOfPhoto].description;
+bigPicture.querySelector('.likes-count').textContent = photosWithData[idOfPhoto].likes;
 
 if (photosWithData[idOfPhoto].comments) {
   const howManyComments = photosWithData[idOfPhoto].comments.length;
-  bigPictureTemplate.querySelector('.comments-count').textContent = howManyComments;
+  bigPicture.querySelector('.comments-count').textContent = howManyComments;
   if (howManyComments <= 5) {
-    bigPictureTemplate.querySelector('.social__comment-count').textContent = `${howManyComments} из ${howManyComments} комментариев`;
+    bigPicture.querySelector('.social__comment-count').textContent = `${howManyComments} из ${howManyComments} комментариев`;
   }
 } else {
-  bigPictureTemplate.querySelector('.social__comment-count').textContent = 'Прокомментируйте первым!';
+  bigPicture.querySelector('.social__comment-count').textContent = 'Прокомментируйте первым!';
 }
-
-// const fragment
-
-// hardComments.remove();//Удаление жестко отрисованных комментарев в разметке
 
 if (comments) {
   comments.forEach((comment) => {
     const commentElement = commentTemplate.cloneNode(true);
     commentElement.querySelector('.social__picture').src = comment.avatar;
+    commentElement.querySelector('.social__picture').alt = comment.name;
     commentElement.querySelector('.social__text').textContent = comment.message;
-    console.log(commentElement);
-    listofCommentsTemplate.append(commentElement);
+    fragment.append(commentElement);
   });
 }
 
+listofCommentsTemplate.append(fragment);
 
-// listofCommentsTemplate.append(fragment);
-
-// commentElement.querySelector('.social__picture').src = 'img/avatar-6.svg';
-// commentElement.querySelector('.social__text').textContent = 'img/avatar-3.svg';
-
-// listofCommentsTemplate.append(commentElement);
-
-
-
-
-// console.log('************************');
-// console.log(photosWithData[0]);
-// console.log(commentTemplate);
-// console.log(comments);
+closingButton.addEventListener('click', () => {
+  bigPicture.classList.add('hidden');
+}, {once: true});
