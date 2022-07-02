b1b82ca8ffe95a23bee3653e0787cc2d1be604c5
ValeraGalera <nuberman88@gmail.com>
2022-07-03 01:56:08 +0400

    добавление файла form.js

---
+++ js/form.js
@@ -0,0 +1,4 @@
+// document.querySelector('.big-picture').classList.add('hidden');
+
+// const form = document.querySelector('.img-upload__overlay');
+// form.classList.remove('hidden');
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {checkLengthText, TEXT as TEXT_FOR_CHECK_LENGTH, MAX_LENGTH} from './check-length-text.js';
 import './render-miniatures.js';
 import './show-big-picture.js';
+import './form.js';
 
-// checkLengthText(TEXT_FOR_CHECK_LENGTH, MAX_LENGTH);
 checkLengthText(TEXT_FOR_CHECK_LENGTH, MAX_LENGTH);
