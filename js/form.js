5a5212668689b0479e9c4161e8321eec6db22458
ValeraGalera <nuberman88@gmail.com>
2022-07-03 20:37:02 +0400

    добавлены файлы для загрузки и редактирования фото

--- js/form.js
+++
@@ -1,4 +0,0 @@
-// document.querySelector('.big-picture').classList.add('hidden');
-
-// const form = document.querySelector('.img-upload__overlay');
-// form.classList.remove('hidden');
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {checkLengthText, TEXT as TEXT_FOR_CHECK_LENGTH, MAX_LENGTH} from './check-length-text.js';
 import './render-miniatures.js';
 import './show-big-picture.js';
-import './form.js';
+import './edit-picture.js';
+import './upload-picture.js';
 
 checkLengthText(TEXT_FOR_CHECK_LENGTH, MAX_LENGTH);
