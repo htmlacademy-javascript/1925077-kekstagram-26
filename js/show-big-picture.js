c35019f50f09fd5b4e80964beb3b7cbbe21c3d64
ValeraGalera <nuberman88@gmail.com>
2022-07-03 23:33:01 +0400

    rename elements

--- js/show-big-picture.js
+++ js/show-big-picture.js
@@ -4,28 +4,29 @@ document.querySelector('body').classList.add('modal-open');
 
 const indexOfPhoto = 0;
 
-const bigPicture = document.querySelector('.big-picture');
-bigPicture.classList.remove('hidden');
+const bigPictureElement = document.querySelector('.big-picture');
+bigPictureElement.classList.remove('hidden');
 
-const listOfCommentsElement = bigPicture.querySelector('.social__comments');
-const commentTemplate = bigPicture.querySelector('.social__comment');
+const listOfCommentsElement = bigPictureElement.querySelector('.social__comments');
+const commentTemplateElement = bigPictureElement.querySelector('.social__comment');
 const comments = photosWithData[indexOfPhoto].comments;
 const fragment = document.createDocumentFragment();
-const closingButton = bigPicture.querySelector('.big-picture__cancel');
+const buttonCloseElement = bigPictureElement.querySelector('.big-picture__cancel');
+
 
 //                заполненение неколлекционными данными
-bigPicture.querySelector('.js-full-img').src = photosWithData[indexOfPhoto].url;
-bigPicture.querySelector('.js-full-img').alt = photosWithData[indexOfPhoto].description;
-bigPicture.querySelector('.social__caption').textContent = photosWithData[indexOfPhoto].description;
-bigPicture.querySelector('.likes-count').textContent = photosWithData[indexOfPhoto].likes;
+bigPictureElement.querySelector('.js-full-img').src = photosWithData[indexOfPhoto].url;
+bigPictureElement.querySelector('.js-full-img').alt = photosWithData[indexOfPhoto].description;
+bigPictureElement.querySelector('.social__caption').textContent = photosWithData[indexOfPhoto].description;
+bigPictureElement.querySelector('.likes-count').textContent = photosWithData[indexOfPhoto].likes;
 if (photosWithData[indexOfPhoto].comments) {
   const howManyComments = photosWithData[indexOfPhoto].comments.length;
-  bigPicture.querySelector('.comments-count').textContent = howManyComments;
+  bigPictureElement.querySelector('.comments-count').textContent = howManyComments;
   if (howManyComments <= 5) {
-    bigPicture.querySelector('.social__comment-count').textContent = `${howManyComments} из ${howManyComments} комментариев`;
+    bigPictureElement.querySelector('.social__comment-count').textContent = `${howManyComments} из ${howManyComments} комментариев`;
   }
 } else {
-  bigPicture.querySelector('.social__comment-count').textContent = 'Прокомментируйте первым!';
+  bigPictureElement.querySelector('.social__comment-count').textContent = 'Прокомментируйте первым!';
 }
 //////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -33,7 +34,7 @@ if (photosWithData[indexOfPhoto].comments) {
 //                 заполнение комментариев
 if (comments) {
   comments.forEach((comment) => {
-    const commentElement = commentTemplate.cloneNode(true);
+    const commentElement = commentTemplateElement.cloneNode(true);
     commentElement.querySelector('.social__picture').src = comment.avatar;
     commentElement.querySelector('.social__picture').alt = comment.name;
     commentElement.querySelector('.social__text').textContent = comment.message;
@@ -45,14 +46,14 @@ listOfCommentsElement.replaceChildren(fragment);
 
 
 //                      закрыть окно с большим фото
-closingButton.addEventListener('click', () => {
-  bigPicture.classList.add('hidden');
+buttonCloseElement.addEventListener('click', () => {
+  bigPictureElement.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
 }, {once: true});
 
 document.addEventListener('keydown', (evt) => {
   if (evt.key === 'Escape') {
-    bigPicture.classList.add('hidden');
+    bigPictureElement.classList.add('hidden');
     document.querySelector('body').classList.remove('modal-open');
   }
 }, {once: true});
@@ -61,7 +62,7 @@ document.addEventListener('keydown', (evt) => {
 
 //
 /////////////////////////////////////////////////////////////////////////////
-bigPicture.querySelector('.social__comment-count').classList.add('hidden');//
+bigPictureElement.querySelector('.social__comment-count').classList.add('hidden');//
 listOfCommentsElement.classList.add('hidden');//      <=========      //////
 /////////          временное скрытие          ///////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
