b4ff55a3d3a78a415706668de6c21c2c4fec1355
Keks <keks@htmlacademy.ru>
2022-07-04 19:58:28 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/show-big-picture.js
+++ 6/js/show-big-picture.js
@@ -1,70 +1,78 @@
 import {photosWithData} from './render-miniatures.js';
 
-document.querySelector('body').classList.add('modal-open');
-
-const indexOfPhoto = 0;
 
 const bigPictureElement = document.querySelector('.big-picture');
-bigPictureElement.classList.remove('hidden');
+
 
 const listOfCommentsElement = bigPictureElement.querySelector('.social__comments');
 const commentTemplateElement = bigPictureElement.querySelector('.social__comment');
-const comments = photosWithData[indexOfPhoto].comments;
 const fragment = document.createDocumentFragment();
 const buttonCloseElement = bigPictureElement.querySelector('.big-picture__cancel');
 
 
-//                заполненение неколлекционными данными
-bigPictureElement.querySelector('.js-full-img').src = photosWithData[indexOfPhoto].url;
-bigPictureElement.querySelector('.js-full-img').alt = photosWithData[indexOfPhoto].description;
-bigPictureElement.querySelector('.social__caption').textContent = photosWithData[indexOfPhoto].description;
-bigPictureElement.querySelector('.likes-count').textContent = photosWithData[indexOfPhoto].likes;
-if (photosWithData[indexOfPhoto].comments) {
-  const howManyComments = photosWithData[indexOfPhoto].comments.length;
-  bigPictureElement.querySelector('.comments-count').textContent = howManyComments;
-  if (howManyComments <= 5) {
-    bigPictureElement.querySelector('.social__comment-count').textContent = `${howManyComments} из ${howManyComments} комментариев`;
-  }
-} else {
-  bigPictureElement.querySelector('.social__comment-count').textContent = 'Прокомментируйте первым!';
-}
-//////////////////////////////////////////////////////////////////////////////////////////////
+const pluckLinksElements = () => {
+  const linksMiniImgContainer = document.querySelector('.pictures');
+
+  const onLinkMiniImgClick = (idOfPhoto) => {
+    for (let i = 0; i < photosWithData.length; i++) {
+      if (photosWithData[i].id === idOfPhoto) {
+        const comments = photosWithData[i].comments;//массив комментов конкретного фото
+
+        //                 заполнение комментариев
+        if (comments) {
+          comments.forEach((comment) => {
+            const commentElement = commentTemplateElement.cloneNode(true);
+            commentElement.querySelector('.social__picture').src = comment.avatar;
+            commentElement.querySelector('.social__picture').alt = comment.name;
+            commentElement.querySelector('.social__text').textContent = comment.message;
+            fragment.append(commentElement);
+          });
+        }
+        listOfCommentsElement.replaceChildren(fragment);
 
 
-//                 заполнение комментариев
-if (comments) {
-  comments.forEach((comment) => {
-    const commentElement = commentTemplateElement.cloneNode(true);
-    commentElement.querySelector('.social__picture').src = comment.avatar;
-    commentElement.querySelector('.social__picture').alt = comment.name;
-    commentElement.querySelector('.social__text').textContent = comment.message;
-    fragment.append(commentElement);
+        //                заполненение неколлекционными данными
+        bigPictureElement.querySelector('.js-full-img').src = photosWithData[i].url;
+        bigPictureElement.querySelector('.js-full-img').alt = photosWithData[i].description;
+        bigPictureElement.querySelector('.social__caption').textContent = photosWithData[i].description;
+        bigPictureElement.querySelector('.likes-count').textContent = photosWithData[i].likes;
+        if (photosWithData[i].comments) {
+          const howManyComments = (photosWithData[i].comments.length);
+          if ((howManyComments) > 5) {
+            bigPictureElement.querySelector('.social__comment-count').textContent = `5 из ${howManyComments} комментариев`;
+          }
+          else if ((howManyComments) <= 5) {
+            bigPictureElement.querySelector('.social__comment-count').textContent = `${howManyComments} из ${howManyComments} комментариев`;
+          }
+        } else /* if (photosWithData[i].comments === null) */ {
+          bigPictureElement.querySelector('.social__comment-count').textContent = 'Прокомментируйте первым!';
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        bigPictureElement.classList.remove('hidden');
+      }
+    }
+  };
+
+  linksMiniImgContainer.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('picture__img')) {
+      onLinkMiniImgClick(+(evt.target.getAttribute('id')));
+    }
   });
-}
-listOfCommentsElement.replaceChildren(fragment);
-//////////////////////////////////////////////////////////////////
+};
+
+pluckLinksElements();
 
 
 //                      закрыть окно с большим фото
 buttonCloseElement.addEventListener('click', () => {
   bigPictureElement.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
-}, {once: true});
+});
 
 document.addEventListener('keydown', (evt) => {
   if (evt.key === 'Escape') {
     bigPictureElement.classList.add('hidden');
     document.querySelector('body').classList.remove('modal-open');
   }
-}, {once: true});
+});
 //////////////////////////////////////////////////////////////////
-
-
-//
-/////////////////////////////////////////////////////////////////////////////
-bigPictureElement.querySelector('.social__comment-count').classList.add('hidden');//
-listOfCommentsElement.classList.add('hidden');//      <=========      //////
-/////////          временное скрытие          ///////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
-//
-
