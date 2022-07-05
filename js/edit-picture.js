56323864f355ee2a3738fbe688d9810048eb9560
ValeraGalera <nuberman88@gmail.com>
2022-07-05 21:22:35 +0400

    action form html

--- js/edit-picture.js
+++ js/edit-picture.js
@@ -1,33 +1,30 @@
-// // document.querySelector('.big-picture').classList.add('hidden');
+// document.querySelector('.big-picture').classList.add('hidden');
 
-// const formOverlayElement = document.querySelector('.img-upload__overlay');
-// const formElement = document.querySelector('.img-upload__form');
-// // const buttonSubmitElement = formOverlayElement.querySelector('.img-upload__submit');
-// const buttonCloseElement = formOverlayElement.querySelector('.img-upload__cancel');
+const formOverlayElement = document.querySelector('.img-upload__overlay');
+const formElement = document.querySelector('.img-upload__form');
+// const buttonSubmitElement = formOverlayElement.querySelector('.img-upload__submit');
+const buttonCloseElement = formOverlayElement.querySelector('.img-upload__cancel');
 
-// // formOverlayElement.classList.remove('hidden');
-
-// //                       прописать в разметке
-// formElement.action = 'https://26.javascript.pages.academy/kekstagram';
+// formOverlayElement.classList.remove('hidden');
 
 
-// // buttonSubmitElement.addEventListener('click', (evt) => {
-// //   evt.preventDefault();
-// // });
+// buttonSubmitElement.addEventListener('click', (evt) => {
+//   evt.preventDefault();
+// });
 
-// // console.log(formOverlayElement);
-// // console.log(formElement);
+// console.log(formOverlayElement);
+// console.log(formElement);
 
-// //                      закрыть окно редактирования фото
-// buttonCloseElement.addEventListener('click', () => {
-//   formOverlayElement.classList.add('hidden');
-//   document.querySelector('body').classList.remove('modal-open');
-// }, {once: true});
+//                      закрыть окно редактирования фото
+buttonCloseElement.addEventListener('click', () => {
+  formOverlayElement.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+}, {once: true});
 
-// document.addEventListener('keydown', (evt) => {
-//   if (evt.key === 'Escape') {
-//     formOverlayElement.classList.add('hidden');
-//     document.querySelector('body').classList.remove('modal-open');
-//   }
-// }, {once: true});
-// //////////////////////////////////////////////////////////////////
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    formOverlayElement.classList.add('hidden');
+    document.querySelector('body').classList.remove('modal-open');
+  }
+}, {once: true});
+//////////////////////////////////////////////////////////////////
